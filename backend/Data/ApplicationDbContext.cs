66ebb9af32b4068dcda071f5856ba86aeda4f751
Tryndex <danielgrabczewski@gmail.com>
2024-09-12 09:32:07 +1200

    Initial commit for Ticketer project with backend setup

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet for Ticket entity
+        public DbSet<Ticket> Tickets { get; set; }
+
+        // You can add other DbSets for more entities here
+    }
+}
