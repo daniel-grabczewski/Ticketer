cbd38c4ca0218b65d8c9fd38eb25b28c4a6430cd
daniel <danielgrabczewski@gmail.com>
2024-10-09 14:00:51 +1300

    Create UpdateTicketDto model for recieving updated ticket in API

--- backend/Models/Ticket.cs
+++ backend/Models/Ticket.cs
@@ -8,4 +8,10 @@ public class Ticket
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
+
+public class UpdateTicketDto {
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
 }
