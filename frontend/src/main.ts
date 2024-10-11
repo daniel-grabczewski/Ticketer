090464e63c528f10a8585cc15faea4fed6d42c24
Tryndex <danielgrabczewski@gmail.com>
2024-10-11 13:16:47 +1300

    Configure main.ts to work with Auth0

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,39 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app/app-routing.module';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAuth0, authHttpInterceptorFn } from '@auth0/auth0-angular';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authHttpInterceptorFn])),
+    provideAnimationsAsync(),
+    provideAuth0({
+      domain: environment.auth.domain,
+      clientId: environment.auth.clientId,
+      authorizationParams: {
+        audience: environment.auth.audience,
+        scope: 'read:tickets create:tickets update:tickets delete:tickets',
+        redirect_uri: window.location.origin,
+      },
+      httpInterceptor: {
+        allowedList: [
+          {
+            uri: `${environment.baseURL}/*`,
+            tokenOptions: {
+              authorizationParams: {
+                audience: environment.auth.audience,
+                scope:
+                  'read:tickets create:tickets update:tickets delete:tickets',
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
 }).catch((err) => console.error(err));
