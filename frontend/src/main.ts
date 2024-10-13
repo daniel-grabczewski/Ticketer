54dec1bc3453383d186c2f9808cd758c083c2d95
Tryndex <danielgrabczewski@gmail.com>
2024-10-14 12:05:18 +1300

    Modify main.ts to use a custom HTTP interceptor
    This means we do not need to manually AccessTokenSilently() everytime we make a server call to the backend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,15 +2,19 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-routing.module';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideAuth0, authHttpInterceptorFn } from '@auth0/auth0-angular';
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { environment } from './environments/environment';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './app/auth-interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authHttpInterceptorFn])),
+    provideHttpClient(
+      withInterceptorsFromDi() // Include interceptors provided via DI
+    ),
     provideAnimationsAsync(),
     provideAuth0({
       domain: environment.auth.domain,
@@ -20,20 +24,12 @@ bootstrapApplication(AppComponent, {
         scope: 'read:tickets create:tickets update:tickets delete:tickets',
         redirect_uri: window.location.origin,
       },
-      httpInterceptor: {
-        allowedList: [
-          {
-            uri: `${environment.baseURL}/*`,
-            tokenOptions: {
-              authorizationParams: {
-                audience: environment.auth.audience,
-                scope:
-                  'read:tickets create:tickets update:tickets delete:tickets',
-              },
-            },
-          },
-        ],
-      },
     }),
+    // Provide the custom interceptor
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
   ],
 }).catch((err) => console.error(err));
