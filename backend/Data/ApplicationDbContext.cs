ea0341c509d1e2b6cf601b53655a8c0d89cb97ca
Tryndex <danielgrabczewski@gmail.com>
2024-10-14 11:14:50 +1300

    Setup new Users table

--- backend/Data/ApplicationDbContext.cs
+++ backend/Data/ApplicationDbContext.cs
@@ -12,7 +12,7 @@ namespace backend.Data
 
         // DbSet for Ticket entity
         public DbSet<Ticket> Tickets { get; set; }
-
+        public DbSet<User> Users { get; set; }
         // You can add other DbSets for more entities here
     }
 }
---
+++ backend/Models/User.cs
@@ -0,0 +1,8 @@
+namespace backend.Models
+{
+public class User
+{
+    public int Id { get; set; }
+    public required string UserId { get; set; }
+}
+}
