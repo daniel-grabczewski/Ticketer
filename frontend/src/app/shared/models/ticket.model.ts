2953051cde1c0e7bd83651627bb64ea2b68dfc25
daniel <danielgrabczewski@gmail.com>
2024-10-29 12:52:31 +1300

    Create GetTicketDetailsResponse interface

---
+++ frontend/src/app/shared/models/ticket.model.ts
@@ -0,0 +1,10 @@
+interface GetTicketDetailsResponse {
+  id: string;
+  name: string;
+  description: string;
+  position: number;
+  colorId: string;
+  listId: string;
+  listName: string;
+  boardId: string;
+}
