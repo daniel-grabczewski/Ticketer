b903d370559eeebb96feeb0bb667e8b49e139b52
Tryndex <danielgrabczewski@gmail.com>
2024-11-01 12:29:52 +1300

    Create basic version of text-input-submenu component

--- frontend/src/app/shared/components/text-input-submenu/text-input-submenu.component.ts
+++ frontend/src/app/shared/components/text-input-submenu/text-input-submenu.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-text-input-submenu',
-  standalone: true,
-  imports: [],
   templateUrl: './text-input-submenu.component.html',
-  styleUrl: './text-input-submenu.component.scss'
+  styleUrls: ['./text-input-submenu.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
 })
 export class TextInputSubmenuComponent {
+  // Inputs
+  @Input() title: string = '';
+  @Input() textInputHeader: string = '';
+  @Input() placeholderText: string = '';
+  @Input() buttonText: string = '';
 
+  // Outputs
+  @Output() menuAction = new EventEmitter<{ type: string; value: string }>();
+  @Output() close = new EventEmitter<void>();
+
+  // Component State
+  textInputValue: string = '';
+
+  // Handle action button click
+  onActionClicked() {
+    // Emit the menu action with the specified structure
+    this.menuAction.emit({
+      type: 'text-input-submenu',
+      value: this.textInputValue,
+    });
+    // Close the submenu
+    this.close.emit();
+  }
+
+  // Handle close button click
+  onCloseClicked() {
+    this.close.emit();
+  }
 }
