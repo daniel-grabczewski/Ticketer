2b976b54ef08803bf1594a885453d59e9a3e2f9c
Tryndex <danielgrabczewski@gmail.com>
2024-11-01 12:29:35 +1300

    Create basic version of confirmation-submenu component

--- frontend/src/app/shared/components/confirmation-submenu/confirmation-submenu.component.ts
+++ frontend/src/app/shared/components/confirmation-submenu/confirmation-submenu.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-confirmation-submenu',
-  standalone: true,
-  imports: [],
   templateUrl: './confirmation-submenu.component.html',
-  styleUrl: './confirmation-submenu.component.scss'
+  styleUrls: ['./confirmation-submenu.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class ConfirmationSubmenuComponent {
+  // Inputs
+  @Input() title: string = '';
+  @Input() confirmationMessage: string = '';
+  @Input() buttonText: string = '';
 
+  // Outputs
+  @Output() menuAction = new EventEmitter<{ type: string; value: boolean }>();
+  @Output() close = new EventEmitter<void>();
+
+  // Handle action button click
+  onActionClicked() {
+    // Emit the menu action with the specified structure
+    this.menuAction.emit({
+      type: 'confirmation-submenu',
+      value: true,
+    });
+    // Close the submenu
+    this.close.emit();
+  }
+
+  // Handle close button click
+  onCloseClicked() {
+    this.close.emit();
+  }
 }
