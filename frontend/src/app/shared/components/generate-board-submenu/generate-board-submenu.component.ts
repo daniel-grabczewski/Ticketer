47f02771e65a4a566dc312aaa48a8b861f9e4ac9
Tryndex <danielgrabczewski@gmail.com>
2024-11-01 11:18:28 +1300

    Create basic version of generate-board-submenu component

--- frontend/src/app/shared/components/generate-board-submenu/generate-board-submenu.component.ts
+++ frontend/src/app/shared/components/generate-board-submenu/generate-board-submenu.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { BackgroundSelectionPanelComponent } from '../background-selection-panel/background-selection-panel.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-generate-board-submenu',
-  standalone: true,
-  imports: [],
   templateUrl: './generate-board-submenu.component.html',
-  styleUrl: './generate-board-submenu.component.scss'
+  styleUrls: ['./generate-board-submenu.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule, BackgroundSelectionPanelComponent],
 })
 export class GenerateBoardSubmenuComponent {
+  // Inputs
+  @Input() title: string = '';
+  @Input() textInputHeader: string = '';
+  @Input() colorSelectionHeader: string = '';
+  @Input() buttonText: string = '';
+  @Input() selectedColorId: number | null = null;
 
+  // Outputs
+  @Output() menuAction = new EventEmitter<{
+    type: string;
+    value: { name: string; color: number | null };
+  }>();
+  @Output() close = new EventEmitter<void>();
+  //! ADDITIONAL OUTPUT TO RELEVANT COMPONENTS ADD IN FIGMA
+
+  // Component State
+  nameInput: string = '';
+  colorId: number | null = null;
+
+  // Handle color selection from the child component
+  onColorSelected(colorId: number | null) {
+    this.colorId = colorId;
+  }
+
+  // Handle create button click
+  onCreateClicked() {
+    // Emit the menu action with the specified structure
+    this.menuAction.emit({
+      type: 'generate-board-submenu',
+      value: {
+        name: this.nameInput,
+        color: this.colorId,
+      },
+    });
+    // Close the submenu
+    this.close.emit();
+  }
+
+  // Handle quit button click
+  onQuitClicked() {
+    this.close.emit();
+  }
 }
