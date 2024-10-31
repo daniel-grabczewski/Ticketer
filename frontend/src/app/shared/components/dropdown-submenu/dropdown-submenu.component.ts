42a63a10a83bfd156ea67bbf23c92a57e7cf92cf
Tryndex <danielgrabczewski@gmail.com>
2024-11-01 12:47:27 +1300

    Create basic version of dropdown-submenu component

--- frontend/src/app/shared/components/dropdown-submenu/dropdown-submenu.component.ts
+++ frontend/src/app/shared/components/dropdown-submenu/dropdown-submenu.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dropdown-submenu',
-  standalone: true,
-  imports: [],
   templateUrl: './dropdown-submenu.component.html',
-  styleUrl: './dropdown-submenu.component.scss'
+  styleUrls: ['./dropdown-submenu.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
 })
 export class DropdownSubmenuComponent {
+  // Inputs
+  @Input() title: string = '';
+  @Input() dropdownInputHeader: string = '';
+  @Input() dropdownOptions: Array<{ id: string; name: string }> = [];
+  @Input() dropdownPlaceholderText: string = '';
+  @Input() buttonText: string = '';
 
+  // Outputs
+  @Output() menuAction = new EventEmitter<{ type: string; value: { id: string; name: string } }>();
+  @Output() close = new EventEmitter<void>();
+
+  // Component State
+  selectedOption: { id: string; name: string } | null = null;
+
+  // Handle action button click
+  onActionClicked() {
+    if (this.selectedOption) {
+      // Emit the menu action with the specified structure
+      this.menuAction.emit({
+        type: 'dropdown-submenu',
+        value: this.selectedOption,
+      });
+    }
+    // Close the submenu
+    this.close.emit();
+  }
+
+  // Handle close button click
+  onCloseClicked() {
+    this.close.emit();
+  }
 }
