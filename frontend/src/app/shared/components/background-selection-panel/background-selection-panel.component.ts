2fbf9253f5d691b4d6f434c6d403c5722c288d1f
Tryndex <danielgrabczewski@gmail.com>
2024-11-01 10:31:18 +1300

    Add color selection logic to background-selection-panel

--- frontend/src/app/shared/components/background-selection-panel/background-selection-panel.component.ts
+++ frontend/src/app/shared/components/background-selection-panel/background-selection-panel.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { GetColorsResponse } from '../../models/color.model';
-import { ColorService } from '../../../core/services/color.service';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
+import { GetColorsResponse } from '../../models/color.model'
+import { ColorService } from '../../../core/services/color.service'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-background-selection-panel',
@@ -11,25 +11,31 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./background-selection-panel.component.scss']
 })
 export class BackgroundSelectionPanelComponent implements OnInit {
-  colors: GetColorsResponse[] = [] // Variable to store the colors
+  @Input() selectedColorId: number | null = null // Initial selected color ID or null for no selection
+  @Output() colorSelected = new EventEmitter<number | null>() // Emits color ID or null on selection
+
+  colors: GetColorsResponse[] = [] // Variable to store colors
 
   constructor(private colorService: ColorService) {}
 
   ngOnInit() {
     this.colorService.getAllColors().subscribe({
       next: (data) => {
-        this.colors = data; // Store the colors received from the service
-        console.log(this.colors)
+        this.colors = data // Store the colors received from the service
       },
       error: (error) => {
-        console.error('Failed to fetch colors:', error); // Handle any errors here
-      },
-      complete: () => {
-        console.log('Color fetch completed'); // Optional complete callback
+        console.error('Failed to fetch colors:', error) // Handle any errors here
       }
-    });
+    })
   }
 
-
-  
+  onColorClick(colorId: number) {
+    if (this.selectedColorId === colorId) {
+      this.selectedColorId = null // Deselect if the same color is clicked
+      this.colorSelected.emit(null)
+    } else {
+      this.selectedColorId = colorId // Select a new color
+      this.colorSelected.emit(colorId)
+    }
+  }
 }
