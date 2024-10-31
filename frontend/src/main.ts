6c06f584ddb4bfabc2a6c08856d2f72db012e17e
daniel <danielgrabczewski@gmail.com>
2024-10-31 13:33:39 +1300

    Increase scope on provideAuth0 in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-routing.module';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideAuth0 } from '@auth0/auth0-angular';
 import { environment } from './environments/environment';
@@ -21,7 +24,8 @@ bootstrapApplication(AppComponent, {
       clientId: environment.auth.clientId,
       authorizationParams: {
         audience: environment.auth.audience,
-        scope: 'read:tickets create:tickets update:tickets delete:tickets',
+        scope:
+          'openid profile email read:tickets create:tickets update:tickets delete:ticket read:boards create:boards update:boards delete:boards read:lists create:lists update:lists delete:lists read:colors create:colors update:colors delete:colors read:users create:users update:users delete:user',
         redirect_uri: window.location.origin,
       },
     }),
@@ -30,6 +34,7 @@ bootstrapApplication(AppComponent, {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true,
-    }, provideAnimationsAsync(),
+    },
+    provideAnimationsAsync(),
   ],
 }).catch((err) => console.error(err));
