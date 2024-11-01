7181c2218648d11a7448d5e9a57466f588bb8f5c
Tryndex <danielgrabczewski@gmail.com>
2024-11-02 11:14:35 +1300

    Create board-item-submenu component

--- frontend/src/app/shared/components/create-board-item-submenu/create-board-item-submenu.component.ts
+++ frontend/src/app/shared/components/create-board-item-submenu/create-board-item-submenu.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common'
+import {
+  CreateBoardItemSubmenuInput,
+  CreateBoardItemSubmenuOutput,
+} from '../../models/submenuInputOutput.model'
 
 @Component({
   selector: 'app-create-board-item-submenu',
-  standalone: true,
-  imports: [],
   templateUrl: './create-board-item-submenu.component.html',
-  styleUrl: './create-board-item-submenu.component.scss'
+  styleUrls: ['./create-board-item-submenu.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
 })
-export class CreateBoardItemSubmenuComponent {
+export class CreateBoardItemSubmenuComponent implements CreateBoardItemSubmenuInput {
+  // Inputs based on CreateBoardItemSubmenuInput
+  @Input() title: string = ''
+  @Input() placeholderText?: string = '' // Optional placeholder
+  @Input() buttonText: string = ''
 
+  // Outputs
+  @Output() menuAction = new EventEmitter<CreateBoardItemSubmenuOutput>()
+  @Output() close = new EventEmitter<void>()
+
+  // Component State
+  textInputValue: string = ''
+
+  // Handle action button click
+  onActionClicked() {
+    // Emit the menu action with the specified structure
+    this.menuAction.emit({ text: this.textInputValue })
+    // Close the submenu
+    this.close.emit()
+  }
+
+  // Handle close button click
+  onCloseClicked() {
+    this.close.emit()
+  }
 }
