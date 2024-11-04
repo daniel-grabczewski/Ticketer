df3e0783ddb29eadb758fe24d0396c9de6b91f18
daniel <danielgrabczewski@gmail.com>
2024-11-05 11:48:01 +1300

    Create interfaces for ticket component

--- frontend/src/app/shared/models/uniqueComonentInputOutput.model.ts
+++ frontend/src/app/shared/models/uniqueComonentInputOutput.model.ts
@@ -3,3 +3,18 @@ export interface BoardThumbnailInput {
   name: string;
   colorId: number;
 }
+
+export interface TicketInput {
+  id: string;
+  name: string;
+  colorId: number | null;
+  listId: string;
+  position: number;
+  colorMap: { [key: number]: string }; // Map of color IDs to their HEX codes
+}
+
+export interface TicketOutput {
+  id: string;
+  targetListId: string;
+  newPosition: number;
+}
