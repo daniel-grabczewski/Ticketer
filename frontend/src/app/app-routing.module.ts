ea38111735b7107b147a0c1683b48f21d97b1a5b
Tryndex <danielgrabczewski@gmail.com>
2024-11-06 16:56:03 +1300

    Add routes for TicketDetailedViewComponent

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,17 +2,9 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './features/dashboard-page/dashboard/dashboard.component';
 import { ProjectComponent } from './project/project.component';
 import { authGuardFn } from '@auth0/auth0-angular';
-import { BackgroundSelectionPanelComponent } from './shared/components/background-selection-panel/background-selection-panel.component';
-import { GenerateBoardSubmenuComponent } from './shared/components/generate-board-submenu/generate-board-submenu.component';
-import { BackgroundSelectionSubmenuComponent } from './shared/components/background-selection-submenu/background-selection-submenu.component';
-import { ConfirmationSubmenuComponent } from './shared/components/confirmation-submenu/confirmation-submenu.component';
-import { TextInputSubmenuComponent } from './shared/components/text-input-submenu/text-input-submenu.component';
-import { DropdownSubmenuComponent } from './shared/components/dropdown-submenu/dropdown-submenu.component';
-import { ColorSelectionPanelComponent } from './shared/components/color-selection-panel/color-selection-panel.component';
-import { ColorSelectionSubmenuComponent } from './shared/components/color-selection-submenu/color-selection-submenu.component';
-import { CreateBoardItemSubmenuComponent } from './shared/components/create-board-item-submenu/create-board-item-submenu.component';
 import { MenuComponent } from './shared/components/menu/menu.component';
 import { BoardComponent } from './features/board-page/board/board.component';
+import { TicketDetailedViewComponent } from './features/board-page/components/ticket-detailed-view/ticket-detailed-view.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent }, // Public route
@@ -34,12 +26,22 @@ export const routes: Routes = [
   {
     path: 'board/:boardId/:boardNameSlug',
     component: BoardComponent,
-    pathMatch: 'full' // Ensures exact matching
+    pathMatch: 'full', // Ensures exact matching
   },
   {
     path: 'board/:boardId', // Add a separate path without the slug
     component: BoardComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
+  },
+  {
+    path: 'board/:boardId/:boardNameSlug/ticket/:ticketId',
+    component: TicketDetailedViewComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'board/:boardId/ticket/:ticketId', // Add a separate path without the slug
+    component: TicketDetailedViewComponent,
+    pathMatch: 'full',
   },
   { path: '**', redirectTo: '' }, // Redirect unknown paths to home
 ];
