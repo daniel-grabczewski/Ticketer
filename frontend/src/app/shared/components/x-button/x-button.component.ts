67e8f9b7336ed06f4767a8dad492bc28848b4526
Tryndex <danielgrabczewski@gmail.com>
2024-11-08 10:59:59 +1300

    Improve scaling system for x-button component

--- frontend/src/app/shared/components/x-button/x-button.component.ts
+++ frontend/src/app/shared/components/x-button/x-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-x-button',
@@ -7,18 +7,11 @@ import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
   standalone: true,
 })
 export class XButtonComponent {
-  @Input() scale: number = 1 // Default scale
-  @Input() color: string = '#4D566E' // Default color
-  @Input() hoverColor: string = '#1A73E8' // Default hover color
+  @Input() scale: number = 1; // Default scale
+  @Input() color: string = 'var(--neutral-darker)'; // Default color
+  @Input() hoverColor: string = 'var(--background-lighter)'; // Default hover color
 
-
-
-  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
-
-  ngOnInit(): void {
-    // Set the hover color as a CSS variable for the component
-    this.renderer.setStyle(this.elRef.nativeElement, '--hover-color', this.hoverColor);
-    this.renderer.setStyle(this.elRef.nativeElement, '--color', this.color);
+  getStrokeWidth(): number {
+    return 7.4 * Math.sqrt(this.scale); // Adjust the scale function as needed
   }
-
 }
