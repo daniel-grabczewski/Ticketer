f0e6394825a403abdf4b18810f16629ef08b84a6
Tryndex <danielgrabczewski@gmail.com>
2024-11-15 16:18:08 +1300

    Create basic mobile-responsiveness for text-input-submenu

--- frontend/src/app/shared/components/text-input-submenu/text-input-submenu.component.ts
+++ frontend/src/app/shared/components/text-input-submenu/text-input-submenu.component.ts
@@ -1,48 +1,51 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'
-import { FormsModule } from '@angular/forms'
-import { CommonModule } from '@angular/common'
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { X_SCALE_VALUE } from '@constants';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import {
   TextInputSubmenuInput,
   TextInputSubmenuOutput,
-} from '../../models/submenuInputOutput.model'
+} from '../../models/submenuInputOutput.model';
+import { XButtonComponent } from '../x-button/x-button.component';
 
 @Component({
   selector: 'app-text-input-submenu',
   templateUrl: './text-input-submenu.component.html',
   styleUrls: ['./text-input-submenu.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, XButtonComponent],
 })
 export class TextInputSubmenuComponent implements TextInputSubmenuInput {
   // Inputs based on TextInputSubmenuInput
-  @Input() title: string = ''
-  @Input() textInputLabel: string = ''
-  @Input() initialText: string = '' // Optional initial text for prefill
-  @Input() placeholder: string = ''
-  @Input() buttonText: string = ''
+  @Input() title: string = '';
+  @Input() textInputLabel: string = '';
+  @Input() initialText: string = ''; // Optional initial text for prefill
+  @Input() placeholder: string = '';
+  @Input() buttonText: string = '';
 
   // Outputs
-  @Output() menuAction = new EventEmitter<TextInputSubmenuOutput>()
-  @Output() close = new EventEmitter<void>()
+  @Output() menuAction = new EventEmitter<TextInputSubmenuOutput>();
+  @Output() close = new EventEmitter<void>();
 
   // Component State
-  textInputValue: string = ''
+  textInputValue: string = '';
+  xScale = X_SCALE_VALUE;
 
-   ngOnInit() {
+  ngOnInit() {
     // Initialize with initialText if provided
-    this.textInputValue = this.initialText
+    this.textInputValue = this.initialText;
   }
 
   // Handle action button click
   onActionClicked() {
     // Emit the menu action with the specified structure
-    this.menuAction.emit({ text: this.textInputValue })
+    this.menuAction.emit({ text: this.textInputValue });
     // Close the submenu
-    this.close.emit()
+    this.close.emit();
   }
 
   // Handle close button click
   onCloseClicked() {
-    this.close.emit()
+    this.close.emit();
   }
 }
