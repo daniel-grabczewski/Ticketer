80faae2150d8eeb2f99b35de59a3801f4b708c57
Tryndex <danielgrabczewski@gmail.com>
2024-11-15 16:14:00 +1300

    Create basic mobile-responsiveness for color-selection-submenu

--- frontend/src/app/shared/components/color-selection-submenu/color-selection-submenu.component.ts
+++ frontend/src/app/shared/components/color-selection-submenu/color-selection-submenu.component.ts
@@ -1,36 +1,45 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core'
-import { ColorSelectionPanelComponent } from '../color-selection-panel/color-selection-panel.component'
-import { CommonModule } from '@angular/common'
-import { ColorSelectionSubmenuInput, ColorSelectionSubmenuOutput } from '../../models/submenuInputOutput.model'
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ColorSelectionPanelComponent } from '../color-selection-panel/color-selection-panel.component';
+import { CommonModule } from '@angular/common';
+import {
+  ColorSelectionSubmenuInput,
+  ColorSelectionSubmenuOutput,
+} from '../../models/submenuInputOutput.model';
+import { XButtonComponent } from '../x-button/x-button.component';
+import { X_SCALE_VALUE } from '@constants';
 
 @Component({
   selector: 'app-color-selection-submenu',
   templateUrl: './color-selection-submenu.component.html',
   styleUrls: ['./color-selection-submenu.component.scss'],
   standalone: true,
-  imports: [CommonModule, ColorSelectionPanelComponent],
+  imports: [CommonModule, ColorSelectionPanelComponent, XButtonComponent],
 })
-export class ColorSelectionSubmenuComponent implements OnInit, ColorSelectionSubmenuInput {
+export class ColorSelectionSubmenuComponent
+  implements ColorSelectionSubmenuInput
+{
   // Inputs matching ColorSelectionSubmenuInput
-  @Input() title: string = ''
-  @Input() colorId: number | null = null
-  @Input() buttonText: string = ''
+  @Input() title: string = '';
+  @Input() colorId: number | null = null;
+  @Input() buttonText: string = '';
 
   // Outputs for ColorSelectionSubmenuOutput
-  @Output() menuAction = new EventEmitter<ColorSelectionSubmenuOutput>()
-  @Output() close = new EventEmitter<void>()
+  @Output() menuAction = new EventEmitter<ColorSelectionSubmenuOutput>();
+  @Output() close = new EventEmitter<void>();
+
+  xScale = X_SCALE_VALUE;
 
   // Component State
-  selectedColorId: number | null = this.colorId
+  selectedColorId: number | null = null;
 
   ngOnInit() {
     // Initialize selectedColorId
-    this.selectedColorId = this.colorId
+    this.selectedColorId = this.colorId;
   }
 
   // Handle color selection from the child component
   onColorSelected(colorId: number | null) {
-    this.selectedColorId = colorId
+    this.selectedColorId = colorId;
   }
 
   // Handle action button click
@@ -38,12 +47,12 @@ export class ColorSelectionSubmenuComponent implements OnInit, ColorSelectionSub
     // Emit the menu action
     this.menuAction.emit({
       colorId: this.selectedColorId,
-    })
-    this.close.emit()
+    });
+    this.close.emit();
   }
 
   // Handle close button click
   onCloseClicked() {
-    this.close.emit()
+    this.close.emit();
   }
 }
