799a0e453280c2e13ddeb786e0ed81bed4e886d2
Tryndex <danielgrabczewski@gmail.com>
2024-12-04 16:15:55 +1300

    Create basic temp-login-page component

---
+++ frontend/src/app/features/temp-login-page/temp-login-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temp-login-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './temp-login-page.component.html',
+  styleUrl: './temp-login-page.component.scss'
+})
+export class TempLoginPageComponent {
+
+}
