97360c4d15995cc2d6e2e54c72feb05d3b27c07e
Tryndex <danielgrabczewski@gmail.com>
2024-12-04 14:35:17 +1300

    Implement logic for buttons on welcome page

--- frontend/src/app/features/welcome-page/welcome-page.component.ts
+++ frontend/src/app/features/welcome-page/welcome-page.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { AuthService as Auth0Service } from '@auth0/auth0-angular';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [],
+  imports: [
+    
+  ],
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss'
 })
 export class WelcomePageComponent {
+  constructor (
+    public auth0: Auth0Service,
+    public authService : AuthService,
+    private router: Router
+  ){}
 
+  goToDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  login() {
+    this.auth0.loginWithRedirect();
+  }
+
+  continueAsGuest() {
+    this.authService.generateGuestId().subscribe({
+      next: (response) => {
+        console.log('Guest session initiated:', response);  
+        if (response) {
+          //this.goToDashboard() Put this back in when the guest functoinality is fixed
+        }
+        
+      },
+      error: (error) => {
+        console.error('Failed to initiate guest session:', error);
+      },
+    });
+  }
 }
